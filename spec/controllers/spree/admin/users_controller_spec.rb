679bd584c392884b2e45c7df47c4594698b2e939
Vishal <vishal@bluebash.co>
2018-11-15 14:14:22 +0530

    Fixed test cases #2

--- spec/controllers/spree/admin/users_controller_spec.rb
+++ spec/controllers/spree/admin/users_controller_spec.rb
@@ -1,8 +1,13 @@
 describe Spree::Admin::UsersController, type: :controller do
   stub_authorization!
+  let(:admin) { create(:admin_user) }
   let(:user) { create(:user) }
 
   context 'GET #switch_session' do
+    before do
+      allow_any_instance_of(Spree::Admin::BaseController).to receive(:spree_current_user).and_return(admin)
+    end
+
     it 'should switch user session' do
       spree_get :switch_session, id: user.id
       expect(request.env['warden'].user.id).to eq(user.id)
