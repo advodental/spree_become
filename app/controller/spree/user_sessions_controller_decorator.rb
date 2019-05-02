25132ff65a2b50732d2c96c940537cfbc8394a05
Vishal <vishal@bluebash.co>
2019-05-02 16:03:29 +0530

    Fixed redirect to admin panel issue

--- app/controller/spree/user_sessions_controller_decorator.rb
+++ app/controller/spree/user_sessions_controller_decorator.rb
@@ -1,7 +1,7 @@
 Spree::UserSessionsController.class_eval do
   def restore_session
     @imitated_by = Spree::User.find_by(email: session[:imitated_by])
-    redirect_to root_url and return unless @imitated_by
+    redirect_to admin_users_path and return unless @imitated_by
     sign_out spree_current_user
     bypass_sign_in(@imitated_by)
     session.delete(:imitated_by)
