8a2c3fca22e66d4da9b88d5256ff864e48351638
Vishal <vishal@bluebash.co>
2019-09-20 18:51:27 +0530

    Add default redirect path if not stored in session #724

--- app/controller/spree/user_sessions_controller_decorator.rb
+++ app/controller/spree/user_sessions_controller_decorator.rb
@@ -1,12 +1,12 @@
 Spree::UserSessionsController.class_eval do
   def restore_session
     @imitated_by = Spree::User.find_by(email: session[:imitated_by])
-    redirect_to session[:redirect_path] and return unless @imitated_by
+    path = session[:redirect_path] || admin_users_path
+    redirect_to path and return unless @imitated_by
     sign_out spree_current_user
     bypass_sign_in(@imitated_by)
     session.delete(:imitated_by)
     session.delete(:imitating_user_email)
-    redirect_to session[:redirect_path]
-    session.delete(:redirect_path)
+    redirect_to path
   end
 end
